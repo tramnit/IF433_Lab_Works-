5a1bbaa6732fb080342f340aa6cbf6642263f726
Muhammad Rassya <khrassya@gmail.com>
2026-02-09 21:00:11 +0700

    week01: (tugas 1) add legacy CircleCalculator code

--- src/oop_00000138554_MuhammadRassyaKhanayaHartawan/week01/GradingSystem.kt
+++ src/oop_00000138554_MuhammadRassyaKhanayaHartawan/week01/GradingSystem.kt
@@ -1,4 +1,4 @@
-package oop_001_johntor.week01 // Pastikan package sesuai
+package oop_00000138554_MuhammadRassyaKhanayaHartawan.week01 // Pastikan package sesuai
 
 fun main(){
     val name = "John Thor"
