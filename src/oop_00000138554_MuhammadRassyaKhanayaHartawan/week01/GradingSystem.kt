a753be0022b4c02555f1ce374ad4228b20e00468
Muhammad Rassya <khrassya@gmail.com>
2026-02-09 20:28:43 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000138554_MuhammadRassyaKhanayaHartawan/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_001_johntor.week01 // Pastikan package sesuai
+
+fun main(){
+    val name: String = "John Thor"
+    val score: Int = 80
+
+    // Concatenation gaya lama
+    println("Name: " + name + ", Nilai:" + score)
+}
