765368b4b12c6056a18c4e8c8df92f084cc58fdb
Muhammad Rassya <khrassya@gmail.com>
2026-02-11 15:44:51 +0700

    week02: add secondary constructor

--- src/oop_00000138554_MuhammmadRassyaKH_week02/M2ClassF_Praktikum.kt
+++ src/oop_00000138554_MuhammmadRassyaKH_week02/M2ClassF_Praktikum.kt
@@ -7,15 +7,13 @@ class Student(
     var gpa: Double = 0.0
 ) {
 
-    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated")
+    constructor(name: String, nim: String)
+            : this(name, nim, "Non-Matriculated") {
+
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 
     init {
-        if (nim.length != 5) {
-            println("PERINGATAN: NIM harus 5 karakter!")
-        }
-
-        if (name.isBlank()) {
-            println("PERINGATAN: Nama tidak boleh kosong!")
-        }
+        println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
     }
 }
