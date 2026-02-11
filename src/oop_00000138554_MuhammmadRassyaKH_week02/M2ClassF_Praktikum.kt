e103d9f85f222540f8bcb00a1b88df599423c1d4
Muhammad Rassya <khrassya@gmail.com>
2026-02-11 15:20:44 +0700

    week02: add warning validation in init block

--- src/oop_00000138554_MuhammmadRassyaKH_week02/M2ClassF_Praktikum.kt
+++ src/oop_00000138554_MuhammmadRassyaKH_week02/M2ClassF_Praktikum.kt
@@ -5,5 +5,13 @@ class Student (
     val nim: String,
     val major: String,
 ){
-
+    init {
+        //Validasi Sederhana: Cek panjang NIM
+        if (nim.length != 5){
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory")
+        }
+    }
 }
