53aec2c1fa389e10b84ac9ffc07990e876322316
Muhammad Rassya <khrassya@gmail.com>
2026-02-11 15:49:59 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_00000138554_MuhammmadRassyaKH_week02/Loan.kt
@@ -0,0 +1,16 @@
+package oop_00000138554_MuhammmadRassyaKH_week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    var loanDuration: Int = 1
+) {
+
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
