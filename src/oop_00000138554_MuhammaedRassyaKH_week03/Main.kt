1af8a728b6258124661203c27089cd9f4e021adf
Muhammad Rassya <khrassya@gmail.com>
2026-02-18 15:50:17 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000138554_MuhammaedRassyaKH_week03/Main.kt
+++ src/oop_00000138554_MuhammaedRassyaKH_week03/Main.kt
@@ -1,7 +1,16 @@
 package oop_00000138554_MuhammaedRassyaKH_week03
 
 fun main() {
+    //employee
     val e = Employee("Budi")
     println("--- Test Error ---")
     e.salary = 5000
+
+    //weapon
+    val sword = Weapon("Sword")
+    sword.damage = -50  // Warning
+    println(sword.damage)  // 0
+    sword.damage = 9999  // 1000
+    println(sword.tier)  // Legendary
 }
+
