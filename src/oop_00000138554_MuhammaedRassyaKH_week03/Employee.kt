35bc2b54cfc838d18e207eb7c06194d702f4f027
Muhammad Rassya <khrassya@gmail.com>
2026-02-18 15:37:44 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000138554_MuhammaedRassyaKH_week03/Employee.kt
+++ src/oop_00000138554_MuhammaedRassyaKH_week03/Employee.kt
@@ -35,7 +35,6 @@ fun main(){
 
     //2. Test Encapsulation
     e.increasePerformance()
-    //e.performanceRating = 5
 
     //3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
