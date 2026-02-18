ed552a22c3484cee487e5448c27c4c90ac510e98
Muhammad Rassya <khrassya@gmail.com>
2026-02-18 15:38:20 +0700

    week 03: cleanup code

--- src/oop_00000138554_MuhammaedRassyaKH_week03/Employee.kt
+++ src/oop_00000138554_MuhammaedRassyaKH_week03/Employee.kt
@@ -28,15 +28,12 @@ class Employee(val name: String){
 fun main(){
     val e = Employee("Budi")
 
-    //1. Test Validasi Salary
     e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    //2. Test Encapsulation
     e.increasePerformance()
 
-    //3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
 
