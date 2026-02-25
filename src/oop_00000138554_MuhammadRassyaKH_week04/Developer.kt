75377e661f8503275c8ee690cde1ce13e18b6e56
Muhammad Rassya <khrassya@gmail.com>
2026-02-25 15:19:01 +0700

    week04: (task 2) create Developer class

---
+++ src/oop_00000138554_MuhammadRassyaKH_week04/Developer.kt
@@ -0,0 +1,8 @@
+package oop_00000138554_MuhammadRassyaKH_week04
+
+class Developer(name: String, baseSalary: Int, val programmingLanguage: String) : Employee(name, baseSalary) {gi
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+    // calculateBonus tidak di-override agar sesuai bawaan parent [cite: 95]
+}
