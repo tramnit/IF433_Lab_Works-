55bb9530a9ac7c46d8262fd11f75f347b6c6d7de
Muhammad Rassya <khrassya@gmail.com>
2026-02-25 15:02:43 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000138554_MuhammadRassyaKH_week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000138554_MuhammadRassyaKH_week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepede Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTruck()
+    myCar.honk()
+    myCar.accelerate()
+}
