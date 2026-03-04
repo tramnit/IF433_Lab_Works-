e14a32a751064dfbde2c46f5b6ea483a04c1be93
Muhammad Rassya <khrassya@gmail.com>
2026-03-04 15:18:30 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000138554_MuhammadRassyaKH_week05/MathHelper.kt
@@ -0,0 +1,8 @@
+package oop_00000138554_MuhammadRassyaKH_week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int = sisi * sisi
+    fun hitungLuas(panjang: Int, lebar: Int): Int = panjang * lebar
+    fun hitungLuas(jariJari: Double): Double = 3.14 * jariJari * jariJari
+}
+
