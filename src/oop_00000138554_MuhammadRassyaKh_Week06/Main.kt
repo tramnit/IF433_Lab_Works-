aef5fb8da0713e92ea57010c6394463676d1a40a
Muhammad Rassya <khrassya@gmail.com>
2026-03-24 20:53:19 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000138554_MuhammadRassyaKh_Week06/Main.kt
+++ src/oop_00000138554_MuhammadRassyaKh_Week06/Main.kt
@@ -1,2 +1,18 @@
 package oop_00000138554_MuhammadRassyaKh_Week06
 
+fun main() {
+    val lamp = SmartLamp("1", "Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("=== Security Mode ===")
+    hub.activateSecurityMode()
+
+    println("\n=== Turn Off All Devices ===")
+    hub.turnOffAllSwitches()
+}
