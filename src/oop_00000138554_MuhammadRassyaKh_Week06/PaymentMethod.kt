6f1bd3c205247befaff165cfc6d3fe591b5d51fd
Muhammad Rassya <khrassya@gmail.com>
2026-03-24 20:43:12 +0700

    week06: create Gopay and CreditCard

--- src/oop_00000138554_MuhammadRassyaKh_Week06/PaymentMethod.kt
+++ src/oop_00000138554_MuhammadRassyaKh_Week06/PaymentMethod.kt
@@ -3,3 +3,15 @@ package oop_00000138554_MuhammadRassyaKh_Week06
 interface PaymentMethod {
     fun pay(amount: Double)
 }
+
+class Gopay : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Paying $amount using GoPay")
+    }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Paying $amount using Credit Card")
+    }
+}
