75c9ab300f61141f4f65d11444ea0acd961eb92a
Muhammad Rassya <khrassya@gmail.com>
2026-03-24 20:50:15 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000138554_MuhammadRassyaKh_Week06/SmartHomeHub.kt
@@ -0,0 +1,18 @@
+package oop_00000138554_MuhammadRassyaKh_Week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
+
