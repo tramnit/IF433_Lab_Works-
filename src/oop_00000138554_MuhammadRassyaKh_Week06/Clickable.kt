ad9a00070cf247f3ef34c20c5a92fc0058f00301
Muhammad Rassya <khrassya@gmail.com>
2026-03-24 20:32:53 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_00000138554_MuhammadRassyaKh_Week06/Clickable.kt
+++ src/oop_00000138554_MuhammadRassyaKh_Week06/Clickable.kt
@@ -1,7 +1,8 @@
 package oop_00000138554_MuhammadRassyaKh_Week06
 
 interface Clickable {
-    var name: String = "Tombol Rahasia" // ERROR: interface tidak boleh punya backing field
+    var name: String
 
     fun click()
 }
+
