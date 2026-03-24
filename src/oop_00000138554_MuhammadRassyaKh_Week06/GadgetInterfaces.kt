fbb6753e223fa5a010e5d13698b0fa34fb8ffffe
Muhammad Rassya <khrassya@gmail.com>
2026-03-24 20:40:07 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000138554_MuhammadRassyaKh_Week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000138554_MuhammadRassyaKh_Week06
+
+interface Camera {
+    fun takePhoto() {
+        println("Taking photo with Camera")
+    }
+}
+
+interface Phone {
+    fun takePhoto() {
+        println("Taking photo with Phone")
+    }
+}
