9a234bd21d38ba03cddff6e4ba5db4cb6773f028
Muhammad Rassya <khrassya@gmail.com>
2026-04-16 13:44:23 +0700

    week07: (task) implement factory pattern in WeaponForge

---
+++ src/oop_00000138554_MuhammadRassyaKH_week07/WeaponForge.kt
@@ -0,0 +1,12 @@
+package oop_00000138554_MuhammadRassyaKH_week07
+
+class Weapon private constructor(val item: GameItem, val durability: Int) {
+    companion object {
+        fun forgeStarterSword(): Weapon {
+            return Weapon(GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON), 50)
+        }
+        fun forgeEpicSword(): Weapon {
+            return Weapon(GameItem("Pedang Langit Biru", 50, ItemRarity.EPIC), 100)
+        }
+    }
+}
