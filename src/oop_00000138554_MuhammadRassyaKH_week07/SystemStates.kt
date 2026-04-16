26b489444e48673deded174e40e27027a9bfed84
Muhammad Rassya <khrassya@gmail.com>
2026-04-16 13:36:08 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000138554_MuhammadRassyaKH_week07/SystemStates.kt
+++ src/oop_00000138554_MuhammadRassyaKH_week07/SystemStates.kt
@@ -4,3 +4,8 @@ enum class AppState {
     STARTING, RUNNING, STOPPED
 }
 
+sealed class ApiResponse {
+    data class Success (val data: String): ApiResponse()
+    data class Error(val message: String): ApiResponse()
+    object Loading: ApiResponse()
+}
