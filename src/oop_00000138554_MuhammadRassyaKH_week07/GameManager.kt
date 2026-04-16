2ff1c022d04cecf2ab95161e307576a155c49b6f
Muhammad Rassya <khrassya@gmail.com>
2026-04-16 13:41:29 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000138554_MuhammadRassyaKH_week07/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_00000138554_MuhammadRassyaKH_week07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
--- src/oop_00000138554_MuhammadRassyaKH_week07/Main.kt
+++ src/oop_00000138554_MuhammadRassyaKH_week07/Main.kt
@@ -27,9 +27,10 @@ fun main() {
 
     println("\n=== TEST SEALED CLASS ===")
     val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-    // Ini akan error karena Loading belum dihandle
     val uiMessage = when (response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Tampilkan Spinner"
     }
+    println(uiMessage)
 }
