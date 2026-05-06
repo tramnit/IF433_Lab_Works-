9c1c93398f0b959698007cb61ac52ebd489fd6c2
Your Name <you@example.com>
2026-05-06 11:38:32 +0700

    week08: create nested classes for Order

---
+++ src/oop_00000138554_MuhammadRassyaKH_week08/OrderModels.kt
@@ -0,0 +1,14 @@
+package oop_00000138554_MuhammadRassyaKH_week08
+
+data class Address(
+    val city: String?
+)
+
+data class Customer(
+    val address: Address?
+)
+
+data class Order(
+    val customer: Customer?
+)
+
