ec6f762ede128eb2d0e1fa90d6ae8ee96e6f2a4e
Your Name <you@example.com>
2026-05-07 23:33:08 +0700

    week09: (task) extract winning trades using filter

--- src/oop_0000013854_MuhammadRassyaKH_week09/TradingDashboard.kt
+++ src/oop_0000013854_MuhammadRassyaKH_week09/TradingDashboard.kt
@@ -10,4 +10,6 @@ fun main() {
         TradeLog("BTCUSDT", "LONG", 50, 45.0, "CLOSED")
     )
     val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
+    val winningTrades = closedTrades.filter { it.roe > 0 }
+// Commit message: "week09: (task) extract winning trades using filter"
 }
