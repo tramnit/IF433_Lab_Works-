d9bd5beef5e6e416b1011d9b4823c92fb269c675
Your Name <you@example.com>
2026-05-07 23:38:54 +0700

    week09: (task) print top performers using forEach

--- src/oop_0000013854_MuhammadRassyaKH_week09/TradingDashboard.kt
+++ src/oop_0000013854_MuhammadRassyaKH_week09/TradingDashboard.kt
@@ -21,4 +21,6 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println(it) }
 }
