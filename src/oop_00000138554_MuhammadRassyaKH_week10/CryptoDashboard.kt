83745c404814c93aa65b174c2adf810e43505bc2
Your Name <you@example.com>
2026-05-18 14:28:37 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_00000138554_MuhammadRassyaKH_week10/CryptoDashboard.kt
+++ src/oop_00000138554_MuhammadRassyaKH_week10/CryptoDashboard.kt
@@ -6,3 +6,7 @@ fun main() {
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 500.0))
     val response = ApiResponse("200 OK", coinRepo.getAll())
+    println("Response Status: ${response.status}")
+    response.data.forEach {
+        println("Coin: ${it.name}, Balance: ${it.balance}")
+    }
