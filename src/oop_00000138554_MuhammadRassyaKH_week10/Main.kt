89e54ad0ca1c71416fe52f299e953ef5b5a50658
Your Name <you@example.com>
2026-05-18 14:13:42 +0700

    week10: create PairBox class with multiple type parameters

--- src/oop_00000138554_MuhammadRassyaKH_week10/Main.kt
+++ src/oop_00000138554_MuhammadRassyaKH_week10/Main.kt
@@ -2,7 +2,8 @@ package oop_00000138554_MuhammadRassyaKH_week10
 
 fun main() {
     println("=== TEST GENERIC CLASS ===")
-    val intBox = Box (100)
+    val intBox = Box(100)
     val stringBox = Box("Generics in Kotlin")
     println("Isi intBox: ${intBox.value}")
     println("Isi stringBox: ${stringBox.value}")
+}
---
+++ src/oop_00000138554_MuhammadRassyaKH_week10/MultiBox.kt
@@ -0,0 +1,3 @@
+package oop_00000138554_MuhammadRassyaKH_week10
+
+class PairBox<K, V>(val key: K, val value: V)
