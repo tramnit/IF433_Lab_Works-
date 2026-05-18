3b96fb5de914a67daa5e512998b50ba426041be7
Your Name <you@example.com>
2026-05-18 14:21:07 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000138554_MuhammadRassyaKH_week10/WalletRepository.kt
@@ -0,0 +1,12 @@
+package oop_00000138554_MuhammadRassyaKH_week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+    fun getAll(): List<T> {
+        return items
+    }
+}
