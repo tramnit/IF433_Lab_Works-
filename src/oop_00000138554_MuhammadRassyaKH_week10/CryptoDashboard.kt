8c13c946fa2bae7f4461c1d754a53b17d95065b9
Your Name <you@example.com>
2026-05-18 14:28:14 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000138554_MuhammadRassyaKH_week10/CryptoDashboard.kt
+++ src/oop_00000138554_MuhammadRassyaKH_week10/CryptoDashboard.kt
@@ -5,3 +5,4 @@ fun main() {
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 500.0))
+    val response = ApiResponse("200 OK", coinRepo.getAll())
