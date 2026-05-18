116415a2d7c7db20f2d7f9cbb60fd4e5936f371e
Your Name <you@example.com>
2026-05-18 14:27:28 +0700

    week10: (task) populate coin repository

--- src/oop_00000138554_MuhammadRassyaKH_week10/CryptoDashboard.kt
+++ src/oop_00000138554_MuhammadRassyaKH_week10/CryptoDashboard.kt
@@ -2,3 +2,6 @@ package oop_00000138554_MuhammadRassyaKH_week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 500.0))
