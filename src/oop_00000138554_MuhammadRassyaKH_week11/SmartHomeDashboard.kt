df8c88bd5c7a199300deff389115f0e08f15c753
Your Name <you@example.com>
2026-05-20 11:37:53 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000138554_MuhammadRassyaKH_week11/SmartHomeDashboard.kt
+++ src/oop_00000138554_MuhammadRassyaKH_week11/SmartHomeDashboard.kt
@@ -2,14 +2,12 @@ package oop_00000138554_MuhammadRassyaKH_week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
-    // Lanjutan fungsi main()
     SmartDevice("Philips WiZ Living Room", "Lighting").apply {
         isOnline = true
         powerLoad = 12
     }.also {
         homeDevices.add(it)
     }
-    // Lanjutan fungsi main()
     SmartDevice("Ezviz Outdoor", "Camera").apply {
         isOnline = true
         powerLoad = 5
@@ -17,3 +15,8 @@ fun main() {
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+    val acUnit = run {
+        SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+    }
+    homeDevices.add(acUnit)
+    homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
