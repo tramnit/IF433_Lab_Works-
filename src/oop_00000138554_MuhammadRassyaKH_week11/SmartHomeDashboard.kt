722d2df8775f7489299629eada02beaa52681d4c
Your Name <you@example.com>
2026-05-20 11:38:31 +0700

    week11: (task) implement device search with let

--- src/oop_00000138554_MuhammadRassyaKH_week11/SmartHomeDashboard.kt
+++ src/oop_00000138554_MuhammadRassyaKH_week11/SmartHomeDashboard.kt
@@ -20,3 +20,7 @@ fun main() {
     }
     homeDevices.add(acUnit)
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println(it.diagnose())
+    }
