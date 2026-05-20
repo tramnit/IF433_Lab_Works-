1def6d642dfb8a7340101558272d2057a8389936
Your Name <you@example.com>
2026-05-20 11:26:01 +0700

    week11: implement run function for object computation

---
+++ src/oop_00000138554_MuhammadRassyaKH_week11/Main.kt
@@ -0,0 +1,11 @@
+package oop_00000138554_MuhammadRassyaKH_week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
+
---
+++ src/oop_00000138554_MuhammadRassyaKH_week11/ScopeLetRun.kt
@@ -0,0 +1,19 @@
+package oop_00000138554_MuhammadRassyaKH_week11
+
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length // Mengembalikan nilai expression terakhir
+    }
+    println("Panjang nama: $length")
+    // Tambahkan kode berikut ke dalam fungsi main() yang sama di atas
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}")
+        length * 2
+    }
+    println("Hasil kalkulasi run: $result")
+}
