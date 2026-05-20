8f217ef514af82a06f0ddb7556602b91fb08f850
Your Name <you@example.com>
2026-05-20 11:34:34 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000138554_MuhammadRassyaKH_week11/SmartHomeModels.kt
@@ -0,0 +1,9 @@
+package oop_00000138554_MuhammadRassyaKH_week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
+
