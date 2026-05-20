31d1abb71652776fff03d3b688303052d863062a
Your Name <you@example.com>
2026-05-20 11:22:57 +0700

    week11: create string extension functions

---
+++ src/oop_00000138554_MuhammadRassyaKH_week11/StringExtensions.kt
@@ -0,0 +1,11 @@
+package oop_00000138554_MuhammadRassyaKH_week11
+
+// Extension function dengan receiver 'this'
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+// Extension function dengan parameter
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
